042ba63ccbfe471c0a21dcebc544928fa20f0d40
Md. Abdullah Al Mahmud <shaphil.mahmud@gmail.com>
2024-08-03 02:54:15 +0600

    Add Module 04 - Composition with Embedded Types > Lesson 04 - Embedding Interfaces into Interfaces (start)

---
+++ src/module_04/04_embedding_interfaces_into_interfaces/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/koddlers/creating-custom-data-types-go.git/src/module_04/04_embedding_interfaces_into_interfaces/organization"
+)
+
+func main() {
+	p := organization.NewPerson("James", "Wilson", organization.NewSocialSecurityNumber("123-45-6789"))
+
+	err := p.SetTwitterHandler("@jam_wils")
+	fmt.Printf("%T\n", organization.TwitterHandler("test"))
+	if err != nil {
+		fmt.Printf("An error occurred setting twitter handler: %s\n", err.Error())
+	}
+
+	fmt.Println(p.TwitterHandler())
+	fmt.Println(p.TwitterHandler().RedirectUrl())
+	fmt.Println(p.ID())
+	fmt.Println(p.FullName())
+}
