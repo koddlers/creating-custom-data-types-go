af7f4a634eeb70f7cbdbd56f0bcf0a6014c4e733
Md. Abdullah Al Mahmud <shaphil.mahmud@gmail.com>
2024-08-04 03:11:06 +0600

    Add Module 04 - Composition with Embedded Types > Lesson 06 - Two Conflicting Types (start)

---
+++ src/module_04/06_two_conflicting_types/organization/person.go
@@ -0,0 +1,117 @@
+package organization
+
+import (
+	"errors"
+	"fmt"
+	"strings"
+)
+
+type Handler struct {
+	handle string
+	name   string
+}
+
+// this is a type declaration
+type TwitterHandler string
+
+func (th TwitterHandler) RedirectUrl() string {
+	cleanHandler := strings.TrimPrefix(string(th), "@")
+	return fmt.Sprintf("https://www.twitter.com/%s", cleanHandler)
+}
+
+type Identifiable interface {
+	ID() string
+}
+
+type Citizen interface {
+	Identifiable
+	Country() string
+}
+
+type socialSecurityNumber string
+
+func NewSocialSecurityNumber(value string) Citizen {
+	return socialSecurityNumber(value)
+}
+
+func (ssn socialSecurityNumber) ID() string {
+	return string(ssn)
+}
+
+func (ssn socialSecurityNumber) Country() string {
+	return "United States of America"
+}
+
+type europeanUnionIdentifier struct {
+	id      string
+	country string
+}
+
+func NewEuropeanUnionIdentifier(id, country string) Citizen {
+	return europeanUnionIdentifier{
+		id:      id,
+		country: country,
+	}
+}
+
+func (eui europeanUnionIdentifier) ID() string {
+	return eui.id
+}
+
+func (eui europeanUnionIdentifier) Country() string {
+	return fmt.Sprintf("EU: %s", eui.country)
+}
+
+type Name struct {
+	First string
+	last  string
+}
+
+func (p *Name) FullName() string {
+	return fmt.Sprintf("%s %s", p.First, p.last)
+}
+
+type Employee struct {
+	Name
+}
+
+type Person struct {
+	Name
+	First          string
+	last           string
+	twitterHandler TwitterHandler
+	Citizen
+}
+
+func NewPerson(firstName, lastName string, citizen Citizen) Person {
+	return Person{
+		Name: Name{
+			First: firstName,
+			last:  lastName,
+		},
+		Citizen: citizen,
+	}
+}
+
+func (p *Person) ID() string {
+	// the `ID()` function exists on both `Identifiable` and `Person`so, `Person` has
+	// an `ID()` function that is accesible both via itself and the `Identifiable` interface.
+	// Since `ID()` is available via both ways, the one defined on the struct itself
+	// (here `Person`) gets precedence.
+	return fmt.Sprintf("Persons ID: %s", p.First)
+}
+
+func (p *Person) SetTwitterHandler(handler TwitterHandler) error {
+	if len(handler) == 0 {
+		p.twitterHandler = handler
+	} else if !strings.HasPrefix(string(handler), "@") {
+		return errors.New("twitter handler must start with an @ symbol")
+	}
+
+	p.twitterHandler = handler
+	return nil
+}
+
+func (p *Person) TwitterHandler() TwitterHandler {
+	return p.twitterHandler
+}
