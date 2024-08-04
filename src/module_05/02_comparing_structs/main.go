bd973dfab60f75ad9f3062285dea7ceba226d53e
Md. Abdullah Al Mahmud <shaphil.mahmud@gmail.com>
2024-08-04 20:24:31 +0600

    Add Module 05 - Comparable Types > Lesson 02 - Comparing Structs (start)

---
+++ src/module_05/02_comparing_structs/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/koddlers/creating-custom-data-types-go.git/src/module_05/02_comparing_structs/organization"
+)
+
+func main() {
+	p := organization.NewPerson("James", "Wilson", organization.NewEuropeanUnionIdentifier("123-45-6789", "Germany"))
+
+	err := p.SetTwitterHandler("@jam_wils")
+	if err != nil {
+		fmt.Printf("An error occurred setting twitter handler: %s\n", err.Error())
+	}
+	fmt.Printf("%T\n\n", organization.TwitterHandler("test"))
+
+	// conflicting types
+	fmt.Println("Conflicting Types:")
+	p.First = "Collin"
+	fmt.Println("p.First:", p.First)
+	fmt.Println("p.Name.First:", p.Name.First)
+	fmt.Println()
+
+	fmt.Println(p.TwitterHandler())
+	fmt.Println(p.TwitterHandler().RedirectUrl())
+	fmt.Println()
+
+	fmt.Println(p.ID())
+	fmt.Println(p.Country())
+	fmt.Println(p.FullName())
+}
