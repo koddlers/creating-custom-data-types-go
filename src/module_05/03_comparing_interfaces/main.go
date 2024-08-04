d712cc0b6116a1ea1f7ac1efff8d256f935625be
Md. Abdullah Al Mahmud <shaphil.mahmud@gmail.com>
2024-08-04 20:48:25 +0600

    Add Module 05 - Comparable Types > Lesson 03 - Comparing Interfaces (start)

---
+++ src/module_05/03_comparing_interfaces/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/koddlers/creating-custom-data-types-go.git/src/module_05/03_comparing_interfaces/organization"
+)
+
+func main() {
+	p := organization.NewPerson("James", "Wilson", organization.NewEuropeanUnionIdentifier("123-45-6789", "Germany"))
+
+	err := p.SetTwitterHandler("@jam_wils")
+	if err != nil {
+		fmt.Printf("An error occurred setting twitter handler: %s\n", err.Error())
+	}
+	fmt.Printf("%T\n\n", organization.TwitterHandler("test"))
+
+	name1 := Name{First: "James", Last: "Wilson"}
+	name2 := Name{First: "James", Last: "Wilson"}
+	// name3 := OtherName{First: "James", Last: "Wilson"}
+
+	if name1 == name2 {
+		fmt.Println("Names match")
+	}
+
+	// ERROR: cannot compare type `Name` with type `OtherName`
+	// if name1 == name3 {
+	// 	fmt.Println("Names match")
+	// }
+}
+
+type Name struct {
+	First string
+	Last  string
+
+	// the following will not allow equality check
+	// Middle []string
+	// Middle func()
+	// Middle map[string]string
+}
+
+type OtherName struct {
+	First string
+	Last  string
+}
